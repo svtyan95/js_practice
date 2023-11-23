59aca0cf3352ff62109822098115b8544fdf33d8
Svyatoslav Tymchyna <svtyan95@gmail.com>
2023-11-23 13:15:05 -0600

    gggg

---
+++ app.js
@@ -0,0 +1,10 @@
+const n = 4;
+let sum = 0;
+
+
+for (let i = 1; i <= n; i++) { 
+     sum += i;
+     console.log(i)
+}
+
+console.log(sum)
--- main.js
+++
@@ -1,6 +0,0 @@
-let a = 5
-let b = 7
-
-const sum = a + b 
-
-console.log(sum)
