255bbd22b2bb47e0994600d80bc6277356c3a617
Svyatoslav Tymchyna <svtyan95@gmail.com>
2023-11-24 20:12:03 -0600

    fffff
    ffff

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const n = 4;
-let sum = 0;
+// const n = 4;
+// let sum = 0;
 
 
-for (let i = 1; i <= n; i++) { 
-     sum += i;
-     console.log(i)
+// for (let i = 1; i <= n; i++) { 
+//      sum += i;
+//      console.log(i)
+// }
+
+// console.log(sum)
+
+
+
+const m = 5;
+const n = 9;
+for (let i = m; i < n; i++) {
+  console.log(i);
 }
-
-console.log(sum)
