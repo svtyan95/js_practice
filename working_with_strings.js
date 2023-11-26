2f33dc72adc45b0f242a4d7ef1837fb3d2a76a99
Svyatoslav Tymchyna <svtyan95@gmail.com>
2023-11-26 15:31:46 -0600

    ffff
    ffff

---
+++ working_with_strings.js
@@ -0,0 +1,180 @@
+// // get string character
+// const title = 'Mate academy'
+// console.log(title)
+// console.log(title.length)
+// console.log(title.length - 1)
+// console.log(title.length - 4)
+
+// //method add
+// console.log(title.at(-1))
+// console.log(title.charAt(20))
+
+
+
+//sting modification methods
+const title = '  Mate academy  '
+console.log(title, title.length)
+
+//upper,lower case
+//DOWN
+console.log(
+     title.toLowerCase()
+)
+//UP
+console.log(
+     title.toUpperCase()
+)
+
+//trim
+console.log(title)
+console.log(
+     title.trim(), //trimLeft(), trimRight()
+     title.trim().length
+)
+
+// replace
+console.log(
+     title.replace('a', '+')
+)
+
+console.log(
+     title.replaceAll('e', '_')
+)
+
+
+
+//how to find a substring
+
+const text = 'Mate academy';
+const part = 'mAte'
+
+console.log(
+     text.includes(part)
+     
+);
+
+console.log(
+     text.toLowerCase().includes(part.toLowerCase())
+);
+
+console.log(
+     text.startsWith('Ma'),
+     text.endsWith('emy')
+)
+
+console.log(
+     text.indexOf('a'),
+     text.indexOf('a', 2),
+     text.indexOf('a', 6)
+
+)
+
+
+console.log(
+     text.lastIndexOf('a'),
+)
+
+//how to slice a part of a string 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const n = 4;
+// // let sum = 0;
+
+
+// // for (let i = 1; i <= n; i++) { 
+// //      sum += i;
+// //      console.log(i)
+// // }
+
+// // console.log(sum)
+
+
+
+// // const m = 5;
+// // const n = 9;
+// // for (let i = m; i < n; i++) {
+// //   console.log(i);
+// // }
+
+
+// //lesson 1//
+// const title = '   A_polo Star   ';
+
+// console.log(title)
+
+// console.log(title.length);
+
+// console.log(title[title.length - 1 * 6])
+
+// console.log(title.at(7))
+
+// console.log(title.charAt(6))
+
+// //lesson 2//
+
+// console.log(
+//      title.toUpperCase()
+// )
+
+// console.log(
+//      title.toLowerCase()
+// )
+
+// console.log(
+//      title.trim(),
+//      title.trim().length
+// )
+
+// //replace
+
+// console.log(
+//      title.replace('_', 'a')
+// )
+
+// console.log(
+//      title.replaceAll('o', '2')
+// )
+
+// //lesson 3//
+
+// const text = 'Mate academy'
+// const part = 'mAte'
+
+
+// // console.log(
+// //      text.includes(part)
+// // )
+
+// console.log(
+//      text.toLocaleLowerCase().includes(part.toLocaleLowerCase())
+// )
+
+// console.log(
+//      text.startsWith('ac'),
+//      text.endsWith('emy')
+// )
+
+// console.log(
+//      text.indexOf('a'),
+//      text.indexOf('a', 2),
+//      text.indexOf('a', 6),
+// )
+
+
+// console.log(
+//      text.indexOf('emy')
+// )
+
+// //
