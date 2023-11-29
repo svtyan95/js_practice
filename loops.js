c040d44e0e3192ea91476971531f00d053b82a7d
Svyatoslav Tymchyna <svtyan95@gmail.com>
2023-11-29 02:39:42 -0600

    gggg
    gggg

---
+++ loops.js
@@ -0,0 +1,30 @@
+//Ho to increment number
+
+// let myAge = 0;
+
+// myAge = myAge + 1;
+// console.log(`I am ${myAge}`)
+
+// myAge = myAge + 1;
+// console.log(`I am ${myAge}`)
+
+// myAge = myAge + 1;
+// console.log(`I am ${myAge}`)
+
+
+
+let myAge = 0;
+
+myAge += 1;
+console.log(`I am ${myAge}`)
+
+myAge ++;
+console.log(`I am ${myAge}`)
+
+myAge++;
+console.log(`I am ${myAge}`)
+
+
+//+= itss the same as ++ this: myAge = myAge + 1;
+
+          
