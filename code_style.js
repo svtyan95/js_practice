e2ac163d42c4e534572ed49963f1aa37bd6c06b0
Svyatoslav Tymchyna <svtyan95@gmail.com>
2024-01-15 12:34:05 -0600

    ттт
    тттт

---
+++ code_style.js
@@ -0,0 +1,28 @@
+// test:
+// let a = 1;
+// let b = 2;
+
+// let c = a + b
+// console.log(c)
+
+
+// use strict
+// 'use strict'
+// let age = 20;
+
+// let age = 12;
+
+// console.log(age)
+
+
+//how to make text readable
+// const text1 = 'sdgsdgdsgsdgsdgsdgsdg'
+// const text2 = 'HfawfbHhfawfMFWfawfaw'
+// const text3 = 'sd gs d gdsg sdgsdgsdgs  dg'
+// const text2 = 'Hfawf    bHh fawfMW  fawfaw'
+
+
+
+// code formation
+'use strick'
+
